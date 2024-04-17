20e4f70bcbe393610a5906ffdbd729805e894419
Auresorhia <148326143+Auresorhia@users.noreply.github.com>
2024-04-17 19:52:41 +0200

    menu

--- Menu/Setting.js
+++ Menu/Setting.js
@@ -12,29 +12,3 @@ function updateSFXVolume(volume) {
     // Mettre à jour le volume des effets sonores (SFX)
     sfx.volume = volume;
 }
-
-// Ajouter un gestionnaire d'événements pour les boutons de contrôle
-document.addEventListener("DOMContentLoaded", function() {
-    const jumpControlButton = document.getElementById("jumpControlButton");
-    const shootControlButton = document.getElementById("shootControlButton");
-
-    jumpControlButton.addEventListener("click", function() {
-        configureControl("saut");
-    });
-
-    shootControlButton.addEventListener("click", function() {
-        configureControl("tir");
-    });
-});
-
-// Fonction pour configurer le contrôle
-function configureControl(controlType) {
-    // Demander à l'utilisateur de saisir la nouvelle touche ou action pour le contrôle
-    const newControl = prompt("New control " + controlType + ":");
-    // Mettre à jour le texte du bouton avec le nouveau contrôle
-    if (controlType === "saut") {
-        jumpControlButton.textContent = "Saut: " + newControl;
-    } else if (controlType === "tir") {
-        shootControlButton.textContent = "Tir: " + newControl;
-    }
-}
